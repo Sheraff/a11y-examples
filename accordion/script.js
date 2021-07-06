c0d3973591cd8daf2a81e33a356671d985b7eb24
Flo <fpellet@ensc.fr>
2021-07-06 08:11:01 +0200

    Update accordion/script.js

---
+++ accordion/script.js
@@ -0,0 +1,20 @@
+import toggleInternalFocusables from './toggleFocusables.js'
+
+const buttonState = Symbol('accordion expanded state')
+const buttons = Array.from(document.querySelectorAll('button'))
+
+buttons.forEach((button) =>
+	button.addEventListener('click', onClick)
+)
+
+/** @param {MouseEvent} event */
+function onClick(event) {
+	/** @type {HTMLButtonElement} */
+	const button = event.target
+	const content = button.nextElementSibling
+	const state = button[buttonState] ?? true
+	button.setAttribute('aria-expanded', state ? 'true' : 'false')
+	content.setAttribute('aria-hidden', state ? 'false' : 'true')
+	toggleInternalFocusables(content, state)
+	button[buttonState] = !state
+}
